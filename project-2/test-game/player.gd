49aae73d90de6cc02fd953333379ef69037e968f
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-04-19 23:23:02 +0330

    screen clamp added

--- project-2/test-game/player.gd
+++ project-2/test-game/player.gd
@@ -3,14 +3,12 @@ extends Node2D
 @export var speed = 200
 var input_vector =  Vector2.ZERO
 
-
 func _ready():
 	print("player is ready")
 	
 func _process(delta):
 	
 	input_vector = Vector2.ZERO
-	
 	if Input.is_action_pressed("move_up"):
 		input_vector.y -= 1
 		
@@ -25,4 +23,8 @@ func _process(delta):
 	
 	input_vector = input_vector.normalized()
 	position  += input_vector * speed * delta 
+	
+	var screen_size = get_viewport_rect().size # getting the size of the screen
+	position.x = clamp(position.x , 0 , screen_size.x)
+	position.y = clamp(position.y , 0 , screen_size.y)
 	
